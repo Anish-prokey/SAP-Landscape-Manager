b767f1c3111ed32b6ec4dc1305cf958074230bee
Anish <Om.Anish@tyson.com>
2023-08-21 11:50:19 +0530

    All changes done

--- src/components/ProductModal.jsx
+++ src/components/ProductModal.jsx
@@ -8,39 +8,45 @@ const ProductModal = ({ open, handleClose, sapSystemId }) => {
   const [data, setData] = useState([]);
 
   // fetching data from API and show it as table
-
   useEffect(() => {
-    const fetchdata = async () => {
-      const response2 = await axios.get(`https://sapd49.tyson.com/sap/opu/odata/sap/ZAPI_PRDVERS_SRV/SysDetailsSet?$filter=(Sysid eq '${sapSystemId}')&sap-client=100`, {
-        headers: {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://sapd49.tyson.com/sap/opu/odata/sap/ZAPI_PRDVERS_SRV/SysDetailsSet?$filter=(Sysid eq '${sapSystemId}')&sap-client=100`, {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          withCredentials: true,
+          auth: {
 
-          'Content-Type': 'application/json',
-
-          'Access-Control-Allow-Origin': '*',
-
-          'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-
-          // 'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-        },
-
-        withCredentials: true,
-        auth: {
-          ususername: 'ANISHO',
-          password: 'Lowkey@12345',
-        },
-      });
-      setData(response2.data.d.results.map((item) => ({
-
-        ProductName: item.Name,
-        Version: item.Version,
-        Description: item.Descript,
-        ModifiedDate: item.ModDate,
-        ModifiedTime: item.ModTime,
-      })));
+            username: '',
+            password: '',
+          },
+          // Assuming you will handle the authentication server-side,
+          // the auth part is removed here.
+        });
+  
+        console.log(response.data.d.results);
+  
+        setData(response.data.d.results.map((item) => ({
+          ProductName: item.Name,
+          Version: item.Version,
+          Description: item.Descript,
+          ModifiedDate: item.ModDate,
+          ModifiedTime: item.ModTime,
+          InstalledYear: new Date(item.ModDate).getFullYear(),
+          Status: item.Inststatus,
+        })));
+  
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     }
-    if (open) { fetchdata(); }
-
-  }, [sapSystemId, open])
+  
+    if (open) {
+      fetchData();
+    }
+  }, [sapSystemId, open]);
+  
   const columns = [
     { Header: 'Product Name', accessor: 'ProductName' },
     { Header: 'Version', accessor: 'Version', className: 'version-column' },
@@ -50,36 +56,7 @@ const ProductModal = ({ open, handleClose, sapSystemId }) => {
     { Header: 'Installed Year', accessor: 'InstalledYear' },
   ];
 
-  const style = {
-    // position: 'absolute',
-    // top: '50%',
-    // left: '50%',
-    // transform: 'translate(-50%, -50%)',
-    // bgcolor: 'background.paper',
-    // boxShadow: 24,
-    // p: 4,
-    // overflowY: 'auto',
-    // '@media(min-width: 220px)': {
-    //   width: '200px',
-    // },
-    // '@media(min-width: 340px)': {
-    //   width: '310px',
-    // },
-    // '@media(min-width: 460px)': {
-    //   width: '420px',
-    // },
-    // '@media(min-width: 760px)': {
-    //   width: '700px',
-    // },
-    // '@media(min-width: 1000px)': {
-    //   width: '920px',
-    // },
-    // '@media(min-width: 1200px)': {
-    //   width: '1120px',
-    // },
-    // '@media(min-width: 1600px)': {
-    //   width: '1520px',
-    // },
+  const style ={
     position: 'absolute',
     top: '50%',
     left: '50%',
@@ -190,7 +167,7 @@ const TableComponent = memo(({ columns, data }) => {
 
 const VersionStatus = ({ data }) => {
   const activeVersions = data.filter(item => item.Status === '+');
-  const otherVersions = data.filter(item => item.Status !== '+');
+  const otherVersions = data.filter(item => item.Status === '-');
 
   const circleStyle = {
     width: '10px',
@@ -201,25 +178,27 @@ const VersionStatus = ({ data }) => {
 
   const indicationsStyle = {
     position: 'absolute',
-    bottom: '0',
-    right: '0',
-    alignItems: 'flex-end'
+    top: '10px',
+    right: '10px',
+    zIndex: 1,
+    display: 'flex',          // <-- This will make its children inline (side-by-side)
+    flexDirection: 'row',     // Ensure children are in a row
+    alignItems: 'center',     // Vertically align items in the center
   };
 
   return (
     <div style={indicationsStyle}>
-      <div style={{ marginRight: '10px', display: "flex", alignItems: "baseline" }}>
+      <div style={{ display: 'flex', alignItems: 'center', marginRight: '15px' }}>
         <div style={{ ...circleStyle, backgroundColor: 'green' }}></div>
         <span>Active Version</span>
       </div>
-      <div>
-        <div style={{ marginRight: '10px', display: "flex", alignItems: "baseline" }}>
-          <div style={{ ...circleStyle, backgroundColor: 'red' }}></div>
-          <span>Old Version</span>
-        </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ ...circleStyle, backgroundColor: 'red' }}></div>
+        <span>Old Version</span>
       </div>
     </div>
   );
 };
 
+  
 export default ProductModal;
